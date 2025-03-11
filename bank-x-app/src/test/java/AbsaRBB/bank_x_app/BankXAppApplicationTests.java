6e23062f43f66fe02e00265047a26464c80e1676
Emihle Manyoni <emihle.manyoni@entelect.co.za>
2025-03-11 14:09:50 +0200

    Initial commit

---
+++ bank-x-app/src/test/java/AbsaRBB/bank_x_app/BankXAppApplicationTests.java
@@ -0,0 +1,13 @@
+package AbsaRBB.bank_x_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankXAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
