ca06ef941639372d641f5f313cd6a2d6105b4d62
Emihle Manyoni <emihle.manyoni@entelect.co.za>
2025-03-14 08:39:13 +0200

    updated removed helper method reconciliation Entity to include column names

--- bank-x-app/persistence/src/main/java/AbsaRBB/entity/ExternalBankEntity.java
+++ bank-x-app/persistence/src/main/java/AbsaRBB/entity/ExternalBankEntity.java
@@ -11,8 +11,10 @@ import  lombok.*;
 public class ExternalBankEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ExternalBankID")
     private Long externalBankID;
-
+    @Column(name = "BankName")
     private String bankName;
+    @Column(name = "BankCode")
     private String bankCode;
 }
